cca084c0705f9df5e4755db92c832fab7624a688
Ian Maher <ianpmaher@gmail.com>
2024-07-18 00:00:42 -0400

    menu items but trouble with displaying json

--- components/Card.tsx
+++ components/Card.tsx
@@ -14,6 +14,7 @@ const Card: React.FC<CardProps> = ({ variant, children, className }) => {
         primary: "bg-inherit bg-opacity-100 hover:bg-opacity-80 hover:scale-110",
         contacts: " bg-slate-100 p-0 hover:outline hover:outline-blue-400 shadow-2xl w-28 md:h-28 h-24 transition-none",
         pictures: "bg-transparent p-0 md:h-24",
+        menu: "bg-transparent p-0 md:h-24",
     };
 
     return (
